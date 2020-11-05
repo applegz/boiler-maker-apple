0309093bc76daeeb28038e2e25caf30309713bcb
Guoying Zhong <ffamochong@gmail.com>
2020-11-05 09:58:57 -0500

    WIP section react & express

---
+++ server/api/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+//these are examples of each router
+router.use('/users', require('./users'));
+router.use('/puppies', require('./puppies'));
+router.use('/kittens', require('./kittens'));
+
+router.use(function (req, res, next) {
+  const err = new Error('Not found.');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = router;
